6ca618591989e3fd4525dc738511df72c499eb2a
Eugene Burmako <xeno.by@gmail.com>
2012-08-11 19:14:36 +0200

    updated

---
+++ Qwe.java
@@ -0,0 +1,5 @@
+public class Qwe {
+  public static String hello() {
+    return "world";
+  }
+}
--- Test.scala
+++ Test.scala
@@ -2,6 +2,7 @@ import Foobzie._
 
 object Test extends App {
   val y = 2
-  val x = foobzie(y)
+  val x = foobzie(y
+  val z = Qwe.hello
   println(x)
 }
