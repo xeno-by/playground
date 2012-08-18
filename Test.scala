29b2fbe57eadab14d6118abb0ed98faab8f84447
Eugene Burmako <xeno.by@gmail.com>
2012-08-18 08:00:52 +0200

    testing debugging

--- Test.scala
+++ Test.scala
@@ -2,7 +2,14 @@ import Foobzie._
 
 object Test extends App {
   val y = 2
-  val x = foobzie(y
+  foo()
+  val x = foobzie(y)
   val z = Qwe.hello
   println(x)
+
+  def foo() = {
+    val a = 10
+    val b = a * a
+    println(b)
+  }
 }
